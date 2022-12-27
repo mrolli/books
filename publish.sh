12c59a078e19ee02f46fc5a6a85a5286f8f3ca8d
Michael Rolli <michael@rollis.ch>
2022-12-27 07:58:28 +0100

    Add simple publishing script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
-mv Bibliothek/*.html ./
-mv Bibliothek/Images/* ./Images/
-git add .
-git ci -m "Publish current library content"
-git push origin main
+if [ -d Bibliothek ]; then
+  mv Bibliothek/*.html ./
+  mv Bibliothek/Images/* ./Images/
+  rm -rf Bibliothek
+
+  git add .
+  git ci -m "Publish current library content"
+  git push origin main
+fi
+
+rsync -av ./ --exclude=.git  beethoven:books.rollis.ch/
